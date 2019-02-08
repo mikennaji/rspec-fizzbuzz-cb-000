9d78d832bb42b11a40056687254daeb533f19a20
Michael Nnaji <mnnaji3@uwo.ca>
2019-02-08 04:41:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     puts"Fizz" # Go fizz
   end
-  if int % 5 == 0 # if the number int is divisible by 3
+  elsif int % 5 == 0 # if the number int is divisible by 3
     puts"Buzz" # Go fizz
   end
   else int % 3 == 0 and int % 5 ==0 # if the number int is divisible by 3
