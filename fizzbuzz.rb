6e6334c24c09c7a612d83bf952c7eca098694b2c
Michael Nnaji <mnnaji3@uwo.ca>
2019-02-08 04:48:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    puts"Fizz"
-    "Fizz" # Go fizz
-  end
+  
+  
   if int % 5 == 0
     puts"Buzz"
     "Buzz"
